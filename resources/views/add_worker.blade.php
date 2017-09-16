98c888c6fc15d8c2cffd2d7a28202651238c4abf
nikol pein <pascasionicoleb@gmail.com>
2017-09-16 10:30:55 +0800

    employer profile

--- resources/views/add_worker.blade.php
+++ resources/views/add_worker.blade.php
@@ -3,31 +3,21 @@
 @section('content')
 <div class="main-container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-
-        	<img src="{{ asset('uploads/user_profile/user.png') }}"/>
-
-        	<h2>Juan de la Cruz</h2>
-        	<span>Brgy. Sapa Miagao, Iloilo</span>
-
+        <div class="col-md-12">
             <div class="panel panel-default">
-              <h4>Masonry</h4>
-              <p>I know i shouldn't do this but im lazy tempor incididunt ut labore et dolore. </p>
-              <span><strong>Rate:</strong>250 per day</span>
+                <h2 class="headline"><span class="glyphicon glyphicon-bell"></span> Notifications</h2>
+                <ul class="notifications">
+                    <li><p><img class="source" src="{{asset('uploads/selfie.png')}}" alt="user-image"> <a class="text-bold" href="#">Clyde Joshua</a> has asked Juan Dela Cruz to work with him.</p></li>
+                    <li><p><img class="source" src="{{asset('uploads/selfie.png')}}" alt="user-image"> <a class="text-bold" href="#">Clyde Joshua</a> has asked Ruben Nacio to work with him.</p></li>
+                    <li><p><img class="source" src="{{asset('uploads/selfie.png')}}" alt="user-image"> <a class="text-bold" href="#">Clyde Joshua</a> has asked Mako Nim to work with him.</p></li>
+                  <li><p><img class="source" src="{{asset('uploads/selfie.png')}}" alt="user-image"> <a class="text-bold" href="#">Clyde Joshua</a> has asked Jenjen Santos to work with him.</p></li>
+                </ul>
             </div>
-            <div class="panel panel-default">
-              <h4>Carpentry</h4>
-              <p>Furnishing experience. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod I know i shouldn't do this but im lazy tempor incididunt ut labore et dolore. </p>
-              <span><strong>Rate:</strong>250 per day</span>
-            </div>
-        </div>
-        <div class="row">
-          <a class="btn btn-primary" href="{{ url('sendsms') }}">Hire</a>
         </div>
     </div>
 </div>
 @endsection
 
 @push('styles')
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/notifications.css') }}">
 @endpush
--- resources/views/profile/employer.blade.php
+++ resources/views/profile/employer.blade.php
@@ -3,28 +3,49 @@
 @section('content')
 <div class="main-container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12 no-padding">
 
-        	<img src="{{ asset('uploads/user_profile/user.png') }}"/>
-<!-- ask for content on this page-->
-        	<h2>Juana de los Santos</h2>
-        	<span>Brgy. Sapa Miagao, Iloilo</span>
+          <img class="user-image" src="{{ asset('uploads/user.png') }}"/>
 
+          <h2 class="name">Juan Yebe </h2>
+          <span class="address">Brgy. Sapa Miagao, Iloilo</span>
+          <div class="spec">
+            <span><span class="glyphicon glyphicon-star"></span> 5.0</span>
+            <span><span class="glyphicon glyphicon-wrench"></span> 2</span>
+          </div>
+
+          <div class="contact_info">
             <div class="panel panel-default">
-              <h4>Masonry</h4>
-              <p>I know i shouldn't do this but im lazy tempor incididunt ut labore et dolore. </p>
-              <span><strong>Rate:</strong>250 per day</span> 
+              <div class="panel-body">
+                <h4 class="headline">Contact Information</h4>
+                <p class="roboto">09498000524</p>
+              </div>
+            </div>
+
+          <h4 class="headline">Previous Requests:</h4>
+
+          <div class="finished-work">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <p class="commenter"><img src="{{asset('uploads/selfie.png')}}" alt="user-image"><span class="name">May Joy Nonaillada</span></p>
+                <p class="comment">Clyde Joshua has displayed excellence in doing masonry work in our house. I am looking forward to hire in the future and I recommend you to hire him.</p>
+              </div>
             </div>
             <div class="panel panel-default">
-              <h4>Carpentry</h4>
-              <p>Furnishing experience. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod I know i shouldn't do this but im lazy tempor incididunt ut labore et dolore. </p>
-              <span><strong>Rate:</strong>250 per day</span>
+              <div class="panel-body">
+                <p class="commenter"><img src="{{asset('uploads/selfie.png')}}" alt="user-image"> <span class="name">Jeopet Ardales</span></p>
+                <p class="comment">Clyde Joshua has helped us in the carpentry of the shed. It's quite impressing.</p>
+              </div>
             </div>
+          </div>
+        </div>
+        <div class="row">
+          <a class="btn btn-primary" href="{{ url('sendsms') }}">Hire</a>
         </div>
     </div>
 </div>
 @endsection
 
 @push('styles')
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/profile.css') }}">
 @endpush
